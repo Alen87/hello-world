1189438bbf244a001e57be05ff70bb83c2c465b1
Alen Boric <boricalen@live.com>
2022-06-06 23:27:32 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 class Main {
   
   public static void main(String[] args) {
-	  System.out.println("hello world");
-	  
+	  System.out.println("Hello world");
     
   }
   
