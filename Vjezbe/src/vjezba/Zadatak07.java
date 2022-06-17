6fc1d4f6ff960f53642333f2cc910ab3f2c4f34e
Alen Boric <boricalen@live.com>
2022-06-17 21:06:12 +0200

    Zadatak07

--- Vjezbe/src/vjezba/Zadatak07.java
+++ Vjezbe/src/vjezba/Zadatak07.java
@@ -1,5 +1,25 @@
 package vjezba;
 
-public class Zadatak07 {
+import javax.swing.JOptionPane;
 
+//Učitati članove niza. Ispisati članove niza sa parnim indeksom.
+
+public class Zadatak07 {
+	public static void main(String[] args) {
+
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj  clanova  niza"));
+
+		int[] niz = new int[n];
+
+		for (int i = 0; i < niz.length; i++) {
+			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		}
+
+		for (int i = 0; i < niz.length; i++) {
+			if (i % 2 == 0) { 
+				System.out.println("niz[" + i + "]=" + niz[i]);
+			}
+		}
+
+	}
 }
