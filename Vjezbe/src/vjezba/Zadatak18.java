2afc7b4841b6fd18bc9c00f974d8d2df24830cff
Alen Boric <boricalen@live.com>
2022-06-23 22:03:38 +0200

    Zadatak18

---
+++ Vjezbe/src/vjezba/Zadatak18.java
@@ -0,0 +1,27 @@
+package vjezba;
+
+import javax.swing.JOptionPane;
+
+//Napisati program za ispis dvocifrenih brojeva kao na slici sa dvije petlje (za svaki rezultat odvojena petlja):
+
+
+
+public class Zadatak18 {
+public static void main(String[] args) {
+	
+int n=Integer.parseInt(JOptionPane.showInputDialog("Unesi  broj"));
+
+
+for(int i=1;i<=n;i++) {
+	for(int j=0;j<=n;j++) {
+		System.out.print(i+ "" +j + " ");
+	}
+	System.out.println();
+}
+	
+	
+	
+	
+	
+}
+}
