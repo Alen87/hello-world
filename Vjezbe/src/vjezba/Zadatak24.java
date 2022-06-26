6986092c2c4b891b48fb7c27bd412a7c1795bcf4
Alen Boric <boricalen@live.com>
2022-06-26 22:46:03 +0200

    Zadatak24

---
+++ Vjezbe/src/vjezba/Zadatak24.java
@@ -0,0 +1,25 @@
+package vjezba;
+
+//Napiši koristeći break, naredbu koja ce ispisati sve moguce kombinacije
+// brojeva 1,2 i 3  uz uvjet da se kombinacija 2-1,2-2,2-3 ne ispišu
+
+public class Zadatak24 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 3; i++) {
+			for (int j = 0; j <= 3; j++) {
+				if (i == 2 && j == 1) {
+					break;
+				} else if (i == 2 && j == 2) {
+					break;
+				} else if (i == 2 && j == 3) {
+                    break;
+				}
+				System.out.println(i + " " + j);
+			}
+
+		}
+
+	}
+
+}
