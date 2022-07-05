f6262ea966e423c94e7b3bcd2b464300daf34109
Alen Boric <boricalen@live.com>
2022-07-05 23:51:25 +0200

    Zadatak29

--- Vjezbe/src/vjezba/Zadatak29.java
+++ Vjezbe/src/vjezba/Zadatak29.java
@@ -1,49 +1,32 @@
 package vjezba;
-import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+//1. Write a Java program to get a number from the user and print whether it is positive or negative. Go to the editor
+//
+//Test Data
+//Input number: 35
+//Expected Output :
+//Number is positive
+
 
 public class Zadatak29 {
 	public static void main(String[] args) {
-		String ime1 = "Alen", ime2 = "Daria";
-
-		String zbrojImena = ime1 + ime2;
-
-		int[] niz = new int[zbrojImena.length()];
-
-		int count = 0;
-
-		for (int i = 0; i < zbrojImena.length(); i++) {
-			count = 0;
-			for (int j = 0; j < zbrojImena.length(); j++) {
-				if (zbrojImena.toLowerCase().charAt(i) == zbrojImena.toLowerCase().charAt(j)) {
-					count++;
-				}
-			}
-			niz[i] = count;
+		
+		int number=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		
+		
+		if(number > 0) {
+			System.out.println("Number is positive");
+		}else {
+			System.out.println("Number is  negative");
 		}
-
-		System.out.println(Arrays.toString(niz));
-
-		// sad imam niz koji je dugaèak imeConcat.length(). i želim ga na dva
-		// dijela prepolovit, tj na Martu i Manuela
-
-		int[] niz1 = new int[ime1.length()];
-		int[] niz2 = new int[ime2.length()];
-
-		for (int i = 0; i < niz.length; i++) {
-
-			if (i < ime1.length()) {
-				niz1[i] = niz[i];
-			} else if (i >= ime1.length()) {
-				niz2[i - ime1.length()] = niz[i];
-			}
-		}
-
-		System.out.println();
-		System.out.print("Niz prvog imena: " + Arrays.toString(niz1));
-		System.out.println();
-		System.out.print("Niz drugog imena: " + Arrays.toString(niz2));
-
-	}
+		
+		
+		
+		
+		
 	}
 
 
+}
