21fc288275fa20b836394da9807e841d898edf35
Alen Boric <boricalen@live.com>
2022-07-16 20:18:36 +0200

    Zadatak16

---
+++ EdunovaZadaci/src/edunova/Zadatak16.java
@@ -0,0 +1,43 @@
+package edunova;
+
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+//za upisani cjeli broj
+	// ispiši zbroj zvih znamenki
+	
+	// ulaz: 263736
+	// izlaz: 27
+	
+	// ulaz: 26
+	// izlaz: 8
+	
+
+public class Zadatak16 {
+	public static void main(String[] args) {
+		
+		
+		int broj=Integer.parseInt(JOptionPane.showInputDialog("Unesi vrijednost broja"));
+		int suma=0;
+		
+		while(broj>0) {
+			suma+=broj%10;
+			 broj/=10;
+			
+			 System.out.println(suma);
+		}
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
