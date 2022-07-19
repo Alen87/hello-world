978785ec29078d5020ce7ce0ca9a2ccd403d582a
Alen Boric <boricalen@live.com>
2022-07-19 21:25:03 +0200

    edunova

--- EdunovaZadaci/src/edunova/Zadatak21.java
+++ EdunovaZadaci/src/edunova/Zadatak21.java
@@ -1,40 +1,29 @@
 package edunova;
 
-//za upisani cjeli broj
-	// ispiši zbroj zvih znamenki
-	
-	// ulaz: 263736
-	// izlaz: 27
-	
-	// ulaz: 26
-	// izlaz: 8
-	
+//Za unesena 2 broja program ispisuje
+		// sumu svih izmedju njih
 
 import javax.swing.JOptionPane;
 
 public class Zadatak21 {
 public static void main(String[] args) {
 
-	
-	int broj=Integer.parseInt(JOptionPane.showInputDialog("Unesi vrijednost"));
+	int a=Integer.parseInt(JOptionPane.showInputDialog("Unesi vrijednost a"));
+	int b=Integer.parseInt(JOptionPane.showInputDialog("Unesi vrijednost b"));
+    
 	int suma=0;
 	
-	 while(broj>0) {
-		 
-		 suma=suma + broj%10;
-		 broj= broj / 10;
-		 
-		 
-	 }
 	
-       System.out.println(suma);	
- 	
+	for(int i=a<=b?a:b;i<=(b>a?b:a);i++) {
+		suma+=i;
+		
+	}
 	
 	
+	System.out.println(suma);
 	
 	
+
+
 }
-
-
-
 }
