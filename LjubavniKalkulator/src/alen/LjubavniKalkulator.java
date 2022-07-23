e89ac9872e10f5932bd18ef3918bdc393ec471b9
Alen Boric <boricalen@live.com>
2022-07-23 18:32:36 +0200

    Update LjubavniKalkulator.java

--- LjubavniKalkulator/src/alen/LjubavniKalkulator.java
+++ LjubavniKalkulator/src/alen/LjubavniKalkulator.java
@@ -1,23 +1,24 @@
 package alen;
 
+
 import java.util.Arrays;
 
-import javax.swing.JOptionPane;
+
 
 public class LjubavniKalkulator {
 	public static void main(String[] args) {
 		String ime1 = "Marta", ime2 = "Manuel";
 
-		String zbrojImena = ime1 + ime2;
+		String zbroj = ime1 + ime2;
 
-		int[] niz = new int[zbrojImena.length()];
+		int[] niz = new int[zbroj.length()];
 
 		int count = 0;
 
-		for (int i = 0; i < zbrojImena.length(); i++) {
+		for (int i = 0; i < zbroj.length(); i++) {
 			count = 0;
-			for (int j = 0; j < zbrojImena.length(); j++) {
-				if (zbrojImena.toLowerCase().charAt(i) == zbrojImena.toLowerCase().charAt(j)) {
+			for (int j = 0; j < zbroj.length(); j++) {
+				if (zbroj.toLowerCase().trim().charAt(i) == zbroj.toLowerCase().trim().charAt(j)) {
 					count++;
 				}
 			}
@@ -44,9 +45,67 @@ public class LjubavniKalkulator {
 		System.out.println();
 		System.out.println(  "    "+ ime1 + "               " + ime2);
 		System.out.print(Arrays.toString(niz1) + "    " + Arrays.toString(niz2));
+		System.out.println();
 		
 		
-      
-	}
+		for (count = zbroj.length() / 2; count > 2; count--) {
 
-}
+			if (zbroj.length() % 2 == 0) {
+				for (int i = 0; i < zbroj.length() / 2; i++) {
+					int broj1 = niz[i];
+					int broj2 = niz[zbroj.length() - 1 - i];
+					int rezultat = broj1 + broj2;
+					System.out.print(rezultat + " ");
+					System.out.println();
+
+				}
+			}else {
+				for (int i = 0; i < zbroj.length() / 2; i++) {
+					int broj1 = niz[i];
+					int broj2 = niz[zbroj.length() - 1 - i];
+					int rezultat = broj1 + broj2;
+				    System.out.print(rezultat + " ");
+					
+				}
+				int sredina = niz[zbroj.length() / 2];
+				System.out.print(sredina);
+				System.out.println();
+
+			}
+			
+					
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+			}
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		 
+     
+
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+
+
