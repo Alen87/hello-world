c74bb19b1ffa6890c78922256a90f4cf1f19877a
Alen Boric <boricalen@live.com>
2022-07-25 18:16:55 +0200

    Update MinutesToYearsAndDaysCalculator.java

--- JavaZadaci/src/alen/MinutesToYearsAndDaysCalculator.java
+++ JavaZadaci/src/alen/MinutesToYearsAndDaysCalculator.java
@@ -2,27 +2,23 @@ package alen;
 
 public class MinutesToYearsAndDaysCalculator {
 	public static void main(String[] args) {
-		
-		
-		printYearsAndDays(561600);
-		
-		
+
+		printYearsAndDays(0);
 
 	}
 
 	public static void printYearsAndDays(long minutes) {
-		if(minutes <0) {
+		if (minutes < 0) {
 			System.out.println("Invalid Value");
+		} else {
+
+			long daystotal = (minutes / 60) / 24;
+			long years = daystotal / 365;
+			long days = daystotal % 365;
+
+			System.out.println(minutes + " min = " + years + " y and " + days + " d");
+
 		}
-
-		long daystotal = (minutes / 60) / 24;
-		long years = daystotal / 365;
-		long days = daystotal % 365;
-		
-		System.out.println(minutes + " min= " + years + "y and " + days+ "d");
-		
-		
-
 	}
 
 }
