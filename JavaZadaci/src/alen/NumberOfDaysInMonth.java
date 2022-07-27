1450656e74371889605c11391b20c487a0ca3b06
Alen Boric <boricalen@live.com>
2022-07-27 12:26:58 +0200

    NumberOfDaysInMonth

--- JavaZadaci/src/alen/NumberOfDaysInMonth.java
+++ JavaZadaci/src/alen/NumberOfDaysInMonth.java
@@ -3,11 +3,11 @@ package alen;
 public class NumberOfDaysInMonth {
 	public static void main(String[] args) {
 
-		System.out.println(daysInMonth(1,2020));
-		System.out.println(daysInMonth(2,2020));
-		System.out.println(daysInMonth(2,2018));
-		System.out.println(daysInMonth(-1,2020));
-		System.out.println(daysInMonth(1,-2020));
+		System.out.println(getDaysInMonth(1,2020));
+		System.out.println(getDaysInMonth(2,2020));
+		System.out.println(getDaysInMonth(2,2018));
+		System.out.println(getDaysInMonth(-1,2020));
+		System.out.println(getDaysInMonth(1,-2020));
 	}
 
 	public static boolean isLeapYear(int year) {
