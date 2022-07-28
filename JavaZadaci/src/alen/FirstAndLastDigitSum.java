79208375364bf429a3a34b4b4279cf43e391f359
Alen Boric <boricalen@live.com>
2022-07-28 22:09:47 +0200

    First And Last Digit Sum

---
+++ JavaZadaci/src/alen/FirstAndLastDigitSum.java
@@ -0,0 +1,31 @@
+package alen;
+
+public class FirstAndLastDigitSum {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println(sumFirstAndLastDigit(435));
+		
+		
+	}
+	
+	public static int sumFirstAndLastDigit(int number) {
+		if(number <0) {
+			return -1;
+		}
+	
+	int firstDigit=0;
+	int lastDigit=number%10;
+	
+	while(number>0) {
+		firstDigit=number%10;
+		number/=10;
+		
+	}
+	System.out.println(firstDigit);
+	return firstDigit + lastDigit;
+	}
+
+}
