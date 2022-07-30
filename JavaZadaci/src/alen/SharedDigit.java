4cdf95ca30b96141bec37b633dcb2f8b8a17b2ac
Alen Boric <boricalen@live.com>
2022-07-30 23:42:05 +0200

    Create SharedDigit.java

---
+++ JavaZadaci/src/alen/SharedDigit.java
@@ -0,0 +1,28 @@
+package alen;
+
+public class SharedDigit {
+
+	public static boolean hasSharedDigit(int number1, int number2) {
+
+		if ((number1 < 10 || number1 > 99) || (number2 < 10 || number2 > 99)) {
+			return false;
+		}
+		int num1FirstDigit = number1 % 10;
+		int num1LastDigit = number1 / 10;
+		int num2FirstDigit = number2 % 10;
+		int num2LastDigit = number2 / 10;
+
+		return num1FirstDigit == num2FirstDigit || num1FirstDigit == num2LastDigit || num1LastDigit == num2FirstDigit
+				|| num1LastDigit == num2LastDigit;
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(hasSharedDigit(12, 23));
+		System.out.println(hasSharedDigit(9, 99));
+		System.out.println(hasSharedDigit(15, 55));
+
+	}
+
+}
