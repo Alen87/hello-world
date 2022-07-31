4b263bb574e84ee172af53cb0209d548fdece153
Alen Boric <boricalen@live.com>
2022-08-01 00:12:37 +0200

    Update LastDigitChecker.java

--- JavaZadaci/src/alen/LastDigitChecker.java
+++ JavaZadaci/src/alen/LastDigitChecker.java
@@ -1,11 +1,40 @@
 package alen;
 
 public class LastDigitChecker {
-	public static hasSameLastDigit(int a,int b,int c) {
+	public static boolean hasSameLastDigit(int a, int b, int c) {
+
+		if ((a >= 10 && a <= 1000) && (b >= 10 && b <= 1000) && (c >= 10 && c <= 1000)) {
+			if ((a % 10 == b % 10) || (b % 10 == c % 10) || (a % 10 == c % 10)) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	public static boolean isValid(int number) {
+		if(number >=10 && number <=1000) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(hasSameLastDigit(41,22,71));
+		System.out.println(hasSameLastDigit(23,32,42));
+		System.out.println(hasSameLastDigit(9,99,999));
+		
+		System.out.println(isValid(10));
+		System.out.println(isValid(468));
+		System.out.println(isValid(1051));
+		
 		
 		
 		
 		
 	}
-
+	
 }
