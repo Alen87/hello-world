b7d913e1fdb750e7a2c48c6bcf23753b1a99336b
Alen Boric <boricalen@live.com>
2022-08-02 00:03:50 +0200

    GreatestCommonDivisor

---
+++ JavaZadaci/src/alen/GreatestCommonDivisior.java
@@ -0,0 +1,30 @@
+package alen;
+
+public class GreatestCommonDivisior {
+
+	public static int getGreatestCommonDivisor(int first,int second) {
+		if((first < 10) || (second<10)) {
+			return-1;
+		}
+		int lower=Math.min(first, second);
+		int remainder=0;
+		for(int i=1;i <=lower;i++ ) {
+			if ((first % i == 0) && (second % i == 0)) {
+				remainder=i;
+			}
+		}
+		
+	
+		return remainder;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(getGreatestCommonDivisor(25, 15));
+		
+	}
+	
+	
+	
+}
