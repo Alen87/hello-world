7424a5824675a882cd626c447e821bd09af8e464
Alen Boric <boricalen@live.com>
2022-08-10 21:22:17 +0200

    Update BarkingDog.java

--- JavaZadaci/src/alen/BarkingDog.java
+++ JavaZadaci/src/alen/BarkingDog.java
@@ -1,21 +1,16 @@
 package alen;
 
 public class BarkingDog {
-public static void main(String[] args) {
-	
-	
-	System.out.println(shouldWakeUp(true,-1));
-	
-}
-	
- public	static boolean shouldWakeUp(boolean barking,int hourOfDay) {
-	 
-	return ((((hourOfDay >=0) && (hourOfDay < 8)) || (hourOfDay > 22) && (hourOfDay<=23)) && (barking)) ?true:false;
-		
-} 
-}
-	 
- 	 
-	 
-	
+	public static void main(String[] args) {
 
+		System.out.println(shouldWakeUp(true, -1));
+
+	}
+
+	public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+		return ((((hourOfDay >= 0) && (hourOfDay < 8)) || (hourOfDay > 22) && (hourOfDay <= 23)) && (barking)) ? true
+				: false;
+
+	}
+}
