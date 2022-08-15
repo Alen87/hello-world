99bb24e840773bc258f30af6064932af02546e1f
Alen Boric <boricalen@live.com>
2022-08-16 00:01:10 +0200

    Bill Burgers

--- JavaZadaci/src/oop_BillBurgers/Start.java
+++ JavaZadaci/src/oop_BillBurgers/Start.java
@@ -3,7 +3,20 @@ package oop_BillBurgers;
 public class Start {
 	
 	public Start() {
-		Hamburger hamburger = new Hamburger(, null, null, null);
+		Hamburger hamburger = new Hamburger("Basic", "Sausage","White", 3.56);
+		hamburger.addHamburgerAddition1("Tomato", 0.27);
+		hamburger.addHamburgerAddition2("Lettuce", 0.75);
+		hamburger.addHamburgerAddition3("Cheese", 1.13);
+		System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+		 
+		HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+		healthyBurger.addHamburgerAddition1("Egg", 5.43);
+		healthyBurger.addHealthAddition1("Lentils", 3.41);
+		System.out.println("Total Healthy Burger price is  " + healthyBurger.itemizeHamburger());
+		 
+		DeluxeBurger db = new DeluxeBurger();
+		db.addHamburgerAddition3("Should not do this", 50.53);
+		System.out.println("Total Deluxe Burger price is " + db.itemizeHamburger());
 		
 		
 		
@@ -12,7 +25,7 @@ public class Start {
 	
 	
 	public static void main(String[] args) {
-		
+		new Start();
 	}
 
 }
