b3cf5a8b593e18c6350f18ac31576ec03f1d2413
Alen Boric <boricalen@live.com>
2022-08-15 22:36:53 +0200

    Update Hamburger.java

--- JavaZadaci/src/oop_BillBurgers/Hamburger.java
+++ JavaZadaci/src/oop_BillBurgers/Hamburger.java
@@ -7,6 +7,18 @@ public class Hamburger {
 	private String breadRollType;
 	private Double price;
 	
+	private String addition1Name;
+	private double addition1Price;
+	
+	private String addition2Name;
+	private double addition2Price;
+	
+	private String addition3Name;
+	private double addition3Price;
+	
+	private String addition4Name;
+	private double addition4Price;
+	
 	
 	public Hamburger(String name, String meat, String breadRollType, Double price) {
 		super();
@@ -16,6 +28,34 @@ public class Hamburger {
 		this.price = price;
 	}
 
+	
+	
+	public void addHamburgerAddition1() {
+		System.out.println(this.addition1Name); 
+	}
+
+	public void addHamburgerAddition2() {
+		System.out.println(this.addition2Name); 
+	}
+	
+	public void addHamburgerAddition3() {
+		System.out.println(this.addition3Name); 
+	}
+	
+	public void addHamburgerAddition4() {
+		System.out.println(this.addition4Name); 
+	}
+	
+	
+	public double itemizeHamburger() {
+		if(this.addition1Name !=null) {
+			this.price+=this.ad
+		}
+		
+		
+		
+		
+	}
 	
 	
 	
